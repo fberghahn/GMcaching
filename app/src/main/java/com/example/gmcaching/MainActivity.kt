0878f6f55f522271506a4845adf1d6ceead0421c
fberghahn <florianberghahn@gmail.com>
2022-01-25 12:43:24 +0100

    ui upgrades und navigation

--- app/src/main/java/com/example/gmcaching/MainActivity.kt
+++ app/src/main/java/com/example/gmcaching/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.gmcaching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gmcaching.adapter.ItemAdapter
 import com.example.gmcaching.data.Datasource
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +24,19 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
-        // Use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true)
-    }
+
+      //val button = findViewById(R.id.button1) as Button
+
+//    button.setOnClickListener(object : View.OnClickListener {
+//        override fun onClick(view: View?) {
+//            // Toast.makeText(this@MainActivity, "Viel Erfolg bei der Suche", Toast.LENGTH_SHORT).show() //Toast zum Klick auf den Cache
+//            val intent = Intent(this@MainActivity, MapsActivity::class.java)
+//            startActivity(intent)
+//        }
+    //}
+    //)
 }
+
+    }
+
+
